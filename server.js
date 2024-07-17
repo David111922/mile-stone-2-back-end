4cfc993cdcbb96c817d7380a680fa349759de1c7
Daniel Fernandez <mdanif9728@gmail.com>
2024-07-17 16:52:50 -0700

    The first commit of this new branch

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-// index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './src/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// DEPENDENCIES
+const express = require('express')
+const { Sequelize } = require('sequelize')
+const app = express()
+
+// CONFIGURATION / MIDDLEWARE
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 
+// ROOT
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello from the server!'
+  })
+})
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// Tire CONTROLLERS
+const tireController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
